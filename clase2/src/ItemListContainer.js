ee1ba7b535864afb6d8be17cbd426e9f3249da70
David Malagreca <malagrecadavid@gmail.com>
2024-12-20 12:28:22 -0300

    agrego style

--- clase2/src/ItemListContainer.js
+++ clase2/src/ItemListContainer.js
@@ -1,11 +1,14 @@
 import NavBar from "./NavBar"
-
 const ItemListContainer = ({greeting}) => {
     return (
-        <div className="container text-center">
-            <h1>
-                {greeting}
-            </h1>
+        <div className="container" style={{ height: '100vh' }}>
+            <div className="row justify-content-end align-items-start" style={{ height: '100%' }}>
+                <div className="col-6">
+                    <h1 className="text-center">
+                        {greeting}
+                    </h1>
+                </div>
+            </div>
         </div>
     )
 }
